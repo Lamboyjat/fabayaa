523a73b6c2b860cb7f83fb4a608037a68e98af40
lamboyjat <lamboyjat@gmail.com>
2021-06-01 15:05:38 +0200

    added a slide show on the home page

---
+++ src/data/SliderData.js
@@ -0,0 +1,66 @@
+import ImageOne from '../images/house-1.jpg'
+import ImageTwo from '../images/house-2.jpg'
+import ImageThree from '../images/house-3.jpg'
+import ImageFour from '../images/house-4.jpg'
+import ImageFive from '../images/house-5.jpg'
+export const SliderData = [
+    {
+        title: 'FabaaINN Villa in Abene',
+        price: '####',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'FabaaINN Villa in Abene',
+        price: '####',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'FabaaINN Villa in Abene',
+        price: '####',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'FabaaINN Villa in Abene',
+        price: '####',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    }
+];
+
+// export const SliderDataTwo = [
+//     {
+//         title: 'FabaaINN Villa in Abene',
+//         price: '####',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageFive,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'FabaaINN Villa in Abene',
+//         price: '####',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageSix,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'FabaaINN Villa in Abene',
+//         price: '####',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageSeven,
+//         alt: 'House'
+//     },
+// ];
